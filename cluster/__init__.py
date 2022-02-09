cd52e1271452050861bf49f94614109579d41972
williams-jacquelinec <williams.jacquelinec@gmail.com>
2022-02-09 14:57:41 -0800

    pushing changes

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,3 +1,8 @@
+"""
+BMI203: Biocomputing algorithms Winter 2022
+Assignment 5: Implementation of KMeans and Silhouette Scoring
+"""
+
 from .kmeans import KMeans
 from .silhouette import Silhouette
 from .utils import (
@@ -5,3 +10,4 @@ from .utils import (
         plot_clusters,
         plot_multipanel)
 
+__version__ = '0.1.0'
