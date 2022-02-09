887b10324ffb9a98c10934775c8cda1df97aa16f
williams-jacquelinec <williams.jacquelinec@gmail.com>
2022-02-09 14:59:22 -0800

    pushing changes

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -73,7 +73,7 @@ class KMeans:
 
             # WAY TO BREAK WHILE LOOP:
             # calculating the MSE!
-            mean_squared_error = km.get_error(centroids_old, self.centroid)
+            mean_squared_error = get_error(centroids_old, self.centroid)
             print(mean_squared_error)
             mse_list.append(mean_squared_error)
 
